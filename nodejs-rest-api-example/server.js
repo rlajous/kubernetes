e2f0396a5003d9f3ab9fb3edba33a1d334372980
Gaston Lifschitz <glifschitz@itba.edu.ar>
2021-06-06 16:32:33 -0300

    update main

---
+++ nodejs-rest-api-example/server.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+app.set("port", process.env.PORT || 4000);
+var bodyParser= require('body-parser');
+const { User, validate } = require("./user");
+
+require("./mongodb")();
+app.use(bodyParser.json({
+   limit: '50mb'
+ }));
+ 
+ app.use(bodyParser.urlencoded({
+   limit: '50mb',
+   parameterLimit: 100000,
+   extended: true 
+ }));
+app.get('/', (req, res) => {
+
+   //await new Promise(resolve => setTimeout(resolve, 1000));
+   User.find().then(result => {
+      console.log(result)
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send("Error\n")
+    });
+   
+})
+
+app.get('/:id', (req, res) => {
+   
+   const { id } = req.params;
+   User.findById(id).then(result => {
+      console.log(result)
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send("Error\n")
+    });
+})
+
+app.post('/',  (req, res) => {
+   const { error } = validate(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+   User.find({ email: req.body.email }).then(result => {
+      console.log(result, result.length);
+      if (result.length !== 0) return res.status(400).send("El email ya se encuentra registrado.");   
+      let user = new User(req.body);
+      user.save().then(user => {
+         return res.status(200).send(user);
+      })
+      .catch(err => {
+         console.log(err);
+         res.send("Error\n")
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.send("Error\n")
+    });;
+
+   
+})
+
+// app.put('/', function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'application/json'});
+//    var response = { "response" : "This is PUT method." }
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+// })
+
+// app.delete('/', function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'application/json'});
+//    var response = { "response" : "This is DELETE method." }
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+// })
+
+var server = app.listen(app.get("port"), function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Node.js API app listening at http://%s:%s", host, port)
+
+})
---
+++ nodejs-rest-api-example/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+const Joi = require("joi");
+
+const userSchema = new Schema({
+  name:     { type: String, minlength: 3, required: true, trim: true },
+  lastName: { type: String, minlength: 3, required: true, trim: true },
+  age: { type: Number, required: true }, 
+  email: {
+    type: String,
+    maxlenght: 255,
+    lowercase: true,
+    trim: true
+  },
+
+  timestamp: { type: Date, default: Date.now }
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+function validateUserSchema(user) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    lastName: Joi.string().required(),
+    age: Joi.number().required(),
+    email: Joi.string().required(),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports.User = User;
+module.exports.validate = validateUserSchema;
